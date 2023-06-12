c984e07cc51c5c6aeaff8302ae8ad1a0c2744b86
Hengyu <h3ngyu@gmail.com>
2023-06-12 11:30:21 +0800

    Upd: revert Keyword.find method

--- tasks/daily/keywords/classes.py
+++ tasks/daily/keywords/classes.py
@@ -1,16 +1,9 @@
 from dataclasses import dataclass
 from typing import ClassVar
-from module.ocr.keyword import Keyword, text_to_variable
 
-
-def __compare__(name, keyword):
-    return text_to_variable(name) == text_to_variable(keyword)
+from module.ocr.keyword import Keyword
 
 
 @dataclass
 class DailyQuest(Keyword):
     instances: ClassVar = {}
-
-    @classmethod
-    def _keyword_compare(cls, name: str, keyword: str) -> bool:
-        return text_to_variable(name) == text_to_variable(keyword)

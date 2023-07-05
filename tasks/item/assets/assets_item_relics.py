1def67bac3e7bb42f5fddca147bc39387d3cdcaa
Hengyu <h3ngyu@gmail.com>
2023-07-06 00:29:23 +0800

    Add: Salvage relic

---
+++ tasks/item/assets/assets_item_relics.py
@@ -0,0 +1,75 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+CONFIRM = ButtonWrapper(
+    name='CONFIRM',
+    share=Button(
+        file='./assets/share/item/relics/CONFIRM.png',
+        area=(776, 558, 800, 582),
+        search=(756, 538, 820, 602),
+        color=(93, 88, 76),
+        button=(776, 558, 800, 582),
+    ),
+)
+FIRST_RELIC = ButtonWrapper(
+    name='FIRST_RELIC',
+    share=Button(
+        file='./assets/share/item/relics/FIRST_RELIC.png',
+        area=(434, 160, 530, 260),
+        search=(414, 140, 550, 280),
+        color=(72, 92, 124),
+        button=(434, 160, 530, 260),
+    ),
+)
+GOTO_SALVAGE = ButtonWrapper(
+    name='GOTO_SALVAGE',
+    share=Button(
+        file='./assets/share/item/relics/GOTO_SALVAGE.png',
+        area=(668, 652, 690, 674),
+        search=(648, 632, 710, 694),
+        color=(155, 156, 156),
+        button=(668, 652, 690, 674),
+    ),
+)
+REVERSE_ORDER = ButtonWrapper(
+    name='REVERSE_ORDER',
+    share=Button(
+        file='./assets/share/item/relics/REVERSE_ORDER.png',
+        area=(720, 649, 738, 667),
+        search=(700, 629, 758, 687),
+        color=(179, 180, 180),
+        button=(720, 649, 738, 667),
+    ),
+)
+SALVAGE = ButtonWrapper(
+    name='SALVAGE',
+    share=Button(
+        file='./assets/share/item/relics/SALVAGE.png',
+        area=(1038, 646, 1062, 670),
+        search=(1018, 626, 1082, 690),
+        color=(122, 115, 98),
+        button=(1038, 646, 1062, 670),
+    ),
+)
+TEST = ButtonWrapper(
+    name='TEST',
+    share=Button(
+        file='./assets/share/item/relics/TEST.png',
+        area=(747, 421, 842, 533),
+        search=(727, 401, 862, 553),
+        color=(16, 23, 37),
+        button=(747, 421, 842, 533),
+    ),
+)
+TEST2 = ButtonWrapper(
+    name='TEST2',
+    share=Button(
+        file='./assets/share/item/relics/TEST2.png',
+        area=(42, 144, 52, 154),
+        search=(22, 124, 72, 174),
+        color=(151, 103, 94),
+        button=(42, 144, 52, 154),
+    ),
+)
---
+++ tasks/item/relics.py
@@ -0,0 +1,52 @@
+from module.base.timer import Timer
+from module.logger import logger
+from tasks.item.assets.assets_item_relics import *
+from tasks.item.keywords import KEYWORD_ITEM_TAB
+from tasks.item.ui import ItemUI
+
+
+class RelicsUI(ItemUI):
+    def salvage_relic(self, skip_first_screenshot=True) -> bool:
+        self.item_goto(KEYWORD_ITEM_TAB.Relics)
+        interval = Timer(1)
+        while 1:  # relic tab -> salvage
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if self.appear(REVERSE_ORDER):
+                break
+            if self.appear_then_click(GOTO_SALVAGE):
+                continue
+
+        skip_first_screenshot = True
+        while 1:  # salvage -> first relic selected
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if self.appear(SALVAGE):
+                break
+            if self.appear_then_click(REVERSE_ORDER):
+                continue
+            if interval.reached() and self.appear_then_click(FIRST_RELIC):
+                interval.reset()
+                continue
+
+        skip_first_screenshot = True
+        while 1:  # selected -> rewards claimed
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if self.handle_reward():
+                logger.info("Relic salvaged")
+                break
+            if self.appear_then_click(SALVAGE):
+                continue
+            if self.appear_then_click(CONFIRM):
+                continue
+        return True

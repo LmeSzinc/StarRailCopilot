513e78081a8bb7a9a16b8ccb5f0cd3dda56fe25b
Yinhr <3162443073@qq.com>
2023-12-19 02:01:45 +0800

    Upd: add filter_preset_extract

---
+++ dev_tools/filter_preset_extract.py
@@ -0,0 +1,106 @@
+import os
+
+import pandas as pd
+import textwrap
+
+from tasks.rogue.keywords.classes import RoguePath
+
+
+class PresetFilterGenerator:
+    def __init__(self, file_name: str = None):
+        if file_name is None:
+            file_name = './filter.xlsx'
+        if not os.path.exists(file_name):
+            raise Exception('File not found')
+        self.file = pd.read_excel(file_name, sheet_name=None)
+        self.paths = []
+        self.path_name = {}
+        self.get_paths()
+        self.general = ['强力', '输出', '生存', '功能']
+        self.replace = {'黄色(强力通用)': '强力', '蓝色(通用输出)': '输出', '绿色(通用生存)': '生存',
+                        '红色(通用功能)': '功能'}
+        self.content = {}
+        self.length = 80
+
+    def get_paths(self):
+        for path in RoguePath.instances.values():
+            self.paths.append(path.name)
+            self.path_name[path.name] = path.cn
+
+    def to_list(self, sheet: pd.DataFrame, title: str, sort_name: str = '排序') -> list:
+        name = '祝福' if title != '奇物' else '奇物'
+        sheet_ = sheet[sheet[sort_name] > 0].sort_values(by=sort_name)
+        sheet_ = sheet_[name].tolist()
+
+        if title in self.paths:
+            for key in self.replace.keys():
+                if key in sheet_:
+                    index = sheet_.index(key)
+                    sheet_ = sheet_[:index] + self.content[self.replace[key]] + sheet_[index + 1:]
+
+        sheet_ = list(dict.fromkeys(sheet_))
+
+        remove_list = sheet[sheet[sort_name] == -1][name].tolist()
+        for item in remove_list:
+            if item in sheet_:
+                sheet_.remove(item)
+
+        if title == '奇物' and 'XX火漆' in sheet_:
+            sheet_[sheet_.index('XX火漆')] = sort_name + '火漆'
+
+        if 'random' in sheet_:
+            sheet_.remove('random')
+
+        return sheet_
+
+    def to_str(self, blessing: list, name: str, one_line=False) -> str:
+        if one_line:
+            ret = ' > '.join(blessing + ['random'])
+            return f'    "{name}": "{ret}",\n'
+        ret = f'    "{name}": \"\"\"\n'
+
+        if 'reset' in blessing:
+            re_index = blessing.index('reset')
+            ret += textwrap.fill(' > '.join(blessing[: re_index]), self.length)
+            ret += '\n> reset\n> '
+            ret += textwrap.fill(' > '.join(blessing[re_index + 1:]), self.length)
+
+        else:
+            ret += textwrap.fill(' > '.join(blessing), self.length)
+        ret = ret.replace(' >\n', '\n> ')
+        ret += '\n> random\n\"\"\",\n\n'
+        return ret
+
+    def generate(self):
+        for _ in self.general:
+            self.content[_] = self.to_list(self.file[_], _)
+
+        resonance = {}
+        curio = {}
+        for path in self.paths:
+            path_name = self.path_name[path]
+            self.content[path] = self.to_list(self.file[path_name], path_name)
+            resonance[path] = self.to_list(self.file['回响'][self.file['回响']['命途'] == path_name], '回响')
+            curio[path] = self.to_list(self.file['奇物'], '奇物', sort_name=path_name)
+        self.content['回响'] = resonance
+        self.content['奇物'] = curio
+
+        preset = 'BLESSING_PRESET = {\n'
+        for path in self.paths:
+            preset += self.to_str(self.content[path], path)
+
+        preset += '}\nRESONANCE_PRESET = {\n'
+        for path in self.paths:
+            preset += self.to_str(self.content['回响'][path], path, one_line=True)
+
+        preset += '}\nCURIO_PRESET = {\n'
+        for path in self.paths:
+            preset += self.to_str(self.content['奇物'][path], path)
+        preset += '}'
+
+        with open('./tasks/rogue/blessing/preset.py', 'w', encoding='utf-8') as f:
+            f.write(preset)
+
+
+if __name__ == '__main__':
+    PresetFilterGenerator().generate()

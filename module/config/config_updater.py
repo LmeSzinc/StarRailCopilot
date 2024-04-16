3ad9ac2d25f86b5238dda7e6938731a31b44b558
katze <titanium22@126.com>
2024-04-16 22:49:46 +0800

    show `Rogue.RogueWorld.SimulatedUniverseFarm` in disabled mode only when `Rogue.RogueWorld.WeeklyFarming` is checked

--- module/config/config_updater.py
+++ module/config/config_updater.py
@@ -864,6 +864,8 @@ class ConfigUpdater:
             yield 'Rogue.RogueBlessing.CustomResonanceFilter'
         if deep_get(data, 'Rogue.RogueBlessing.PresetCurioFilter') != 'custom':
             yield 'Rogue.RogueBlessing.CustomCurioFilter'
+        if deep_get(data, 'Rogue.RogueWorld.WeeklyFarming') != True:
+            yield 'Rogue.RogueWorld.SimulatedUniverseFarm'
 
     def get_hidden_args(self, data) -> t.Set[str]:
         """

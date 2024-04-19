fc5dc22dfd448dab26c50c5dd67c554fb8bc3664
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-04-19 18:03:55 +0800

    Fix: Point in tuple cannot be compared to int

--- tasks/rogue/route/exit.py
+++ tasks/rogue/route/exit.py
@@ -112,9 +112,9 @@ class RogueExit(CombatInteract):
         distant_point = np.array((1509.46, 247.34))
         name_y = 77.60
         foot_y = 621.82
-        if point < 80:
+        if point[1] < 80:
             logger.warning(f'screen2direction: Point {point} to high')
-            point[1] = 80
+            point = (point[0], 80)
 
         door_projection_bottom = (
             Points([point]).link(vanish_point).get_x(name_y)[0],

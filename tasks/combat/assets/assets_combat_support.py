9b9e85990ae46fffb5981e7f3c7c7e5add93fb4c
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-05-08 20:38:34 +0800

    Upd: Combat support assets

--- tasks/combat/assets/assets_combat_support.py
+++ tasks/combat/assets/assets_combat_support.py
@@ -17,30 +17,30 @@ COMBAT_SUPPORT_LIST = ButtonWrapper(
     name='COMBAT_SUPPORT_LIST',
     share=Button(
         file='./assets/share/combat/support/COMBAT_SUPPORT_LIST.png',
-        area=(67, 645, 93, 669),
-        search=(47, 625, 113, 689),
-        color=(204, 205, 205),
-        button=(67, 645, 93, 669),
+        area=(39, 645, 65, 669),
+        search=(19, 625, 85, 689),
+        color=(204, 206, 206),
+        button=(39, 645, 65, 669),
     ),
 )
 COMBAT_SUPPORT_LIST_GRID = ButtonWrapper(
     name='COMBAT_SUPPORT_LIST_GRID',
     share=Button(
         file='./assets/share/combat/support/COMBAT_SUPPORT_LIST_GRID.png',
-        area=(64, 158, 155, 627),
-        search=(44, 138, 175, 647),
-        color=(117, 107, 124),
-        button=(64, 158, 155, 627),
+        area=(36, 148, 128, 629),
+        search=(16, 128, 148, 649),
+        color=(135, 124, 133),
+        button=(36, 148, 128, 629),
     ),
 )
 COMBAT_SUPPORT_LIST_SCROLL = ButtonWrapper(
     name='COMBAT_SUPPORT_LIST_SCROLL',
     share=Button(
         file='./assets/share/combat/support/COMBAT_SUPPORT_LIST_SCROLL.png',
-        area=(448, 156, 452, 592),
-        search=(428, 136, 472, 612),
-        color=(130, 133, 153),
-        button=(448, 156, 452, 592),
+        area=(472, 162, 476, 598),
+        search=(452, 142, 496, 618),
+        color=(150, 151, 166),
+        button=(472, 162, 476, 598),
     ),
 )
 COMBAT_SUPPORT_SELECTED = ButtonWrapper(
@@ -59,28 +59,28 @@ SUPPORT_SELECTED = ButtonWrapper(
         Button(
             file='./assets/share/combat/support/SUPPORT_SELECTED.png',
             area=(54, 202, 60, 220),
-            search=(40, 146, 68, 628),
+            search=(15, 148, 45, 629),
             color=(125, 126, 131),
             button=(54, 202, 60, 220),
         ),
         Button(
             file='./assets/share/combat/support/SUPPORT_SELECTED.2.png',
             area=(53, 547, 59, 565),
-            search=(40, 146, 68, 628),
+            search=(15, 148, 45, 629),
             color=(144, 146, 147),
             button=(53, 547, 59, 565),
         ),
         Button(
             file='./assets/share/combat/support/SUPPORT_SELECTED.3.png',
             area=(54, 432, 61, 450),
-            search=(40, 146, 68, 628),
+            search=(15, 148, 45, 629),
             color=(139, 139, 141),
             button=(54, 432, 61, 450),
         ),
         Button(
             file='./assets/share/combat/support/SUPPORT_SELECTED.4.png',
             area=(52, 417, 59, 435),
-            search=(40, 146, 68, 628),
+            search=(15, 148, 45, 629),
             color=(140, 138, 145),
             button=(52, 417, 59, 435),
         ),

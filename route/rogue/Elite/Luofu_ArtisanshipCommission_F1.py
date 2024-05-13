8375e8729d4fbe20ecc107b2acede8f73bc0bc9e
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-05-14 02:17:24 +0800

    Upd: Route copies

--- route/rogue/Elite/Luofu_ArtisanshipCommission_F1.py
+++ route/rogue/Elite/Luofu_ArtisanshipCommission_F1.py
@@ -24,6 +24,36 @@ class Route(RouteBase):
         self.domain_single_exit(exit_)
         # ===== End of generated waypoints =====
 
+    def Luofu_ArtisanshipCommission_F1_X391Y493(self):
+        """
+        | Waypoint | Position                  | Direction | Rotation |
+        | -------- | ------------------------- | --------- | -------- |
+        | spawn    | Waypoint((385.2, 494.6)), | 94.2      | 91       |
+        | enemy    | Waypoint((444.2, 490.5)), | 94.2      | 91       |
+        | reward   | Waypoint((448.6, 497.2)), | 149.7     | 91       |
+        | exit_    | Waypoint((458.0, 483.7)), | 94.2      | 91       |
+        """
+        self.map_init(plane=Luofu_ArtisanshipCommission, floor="F1", position=(391.1, 493.2))
+        enemy = Waypoint((444.2, 490.5))
+        reward = Waypoint((448.6, 497.2))
+        exit_ = Waypoint((458.0, 483.7))
+
+        self.clear_elite(enemy)
+        self.domain_reward(reward)
+        self.domain_single_exit(exit_)
+        # ===== End of generated waypoints =====
+
+        """
+        Notes
+        Luofu_ArtisanshipCommission_F1_X391Y493 is the same as Luofu_ArtisanshipCommission_F1_X385Y494
+        but for wrong spawn point detected
+        """
+        # Best 3 predictions: [
+        # ('Elite_Luofu_ArtisanshipCommission_F1_X385Y494', 0.182, (391.1, 493.2)),
+        # ('Elite_Jarilo_CorridorofFadingEchoes_F1_X415Y933', 0.157, (364.0, 951.0)),
+        # ('Elite_Jarilo_CorridorofFadingEchoes_F1_X415Y947', 0.157, (364.0, 951.0))
+        # ]
+
     def Luofu_ArtisanshipCommission_F1_X504Y493(self):
         """
         | Waypoint | Position                  | Direction | Rotation |

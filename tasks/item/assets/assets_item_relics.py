c31e0a7f40f2a5b9bc11794d6748e9ecd265240d
katze <titanium22@126.com>
2024-05-24 22:24:50 +0800

    Relic salvage and relic filtering

--- tasks/item/assets/assets_item_relics.py
+++ tasks/item/assets/assets_item_relics.py
@@ -3,6 +3,40 @@ from module.base.button import Button, ButtonWrapper
 # This file was auto-generated, do not modify it manually. To generate:
 # ``` python -m dev_tools.button_extract ```
 
+FILTER_CONFIRM = ButtonWrapper(
+    name='FILTER_CONFIRM',
+    cn=Button(
+        file='./assets/cn/item/relics/FILTER_CONFIRM.png',
+        area=(1008, 640, 1248, 688),
+        search=(988, 620, 1268, 708),
+        color=(208, 208, 209),
+        button=(1008, 640, 1248, 688),
+    ),
+    en=Button(
+        file='./assets/en/item/relics/FILTER_CONFIRM.png',
+        area=(1008, 640, 1248, 688),
+        search=(988, 620, 1268, 708),
+        color=(206, 206, 207),
+        button=(1008, 640, 1248, 688),
+    ),
+)
+FILTER_RESET = ButtonWrapper(
+    name='FILTER_RESET',
+    cn=Button(
+        file='./assets/cn/item/relics/FILTER_RESET.png',
+        area=(832, 640, 992, 688),
+        search=(812, 620, 1012, 708),
+        color=(199, 199, 200),
+        button=(832, 640, 992, 688),
+    ),
+    en=Button(
+        file='./assets/en/item/relics/FILTER_RESET.png',
+        area=(832, 640, 992, 688),
+        search=(812, 620, 1012, 708),
+        color=(200, 200, 201),
+        button=(832, 640, 992, 688),
+    ),
+)
 FIRST_RELIC = ButtonWrapper(
     name='FIRST_RELIC',
     share=Button(
@@ -23,6 +57,16 @@ FIRST_RELIC_SELECTED = ButtonWrapper(
         button=(424, 136, 452, 164),
     ),
 )
+GOTO_ENHANCE_FILTER = ButtonWrapper(
+    name='GOTO_ENHANCE_FILTER',
+    share=Button(
+        file='./assets/share/item/relics/GOTO_ENHANCE_FILTER.png',
+        area=(161, 652, 183, 675),
+        search=(141, 632, 203, 695),
+        color=(187, 189, 191),
+        button=(161, 652, 183, 675),
+    ),
+)
 GOTO_SALVAGE = ButtonWrapper(
     name='GOTO_SALVAGE',
     share=Button(
@@ -33,24 +77,24 @@ GOTO_SALVAGE = ButtonWrapper(
         button=(668, 652, 690, 674),
     ),
 )
-ORDER_ASCENDING = ButtonWrapper(
-    name='ORDER_ASCENDING',
+GOTO_SALVAGE_FILTER = ButtonWrapper(
+    name='GOTO_SALVAGE_FILTER',
     share=Button(
-        file='./assets/share/item/relics/ORDER_ASCENDING.png',
-        area=(720, 649, 737, 667),
-        search=(700, 629, 757, 687),
-        color=(176, 176, 178),
-        button=(720, 649, 737, 667),
+        file='./assets/share/item/relics/GOTO_SALVAGE_FILTER.png',
+        area=(443, 647, 465, 670),
+        search=(423, 627, 485, 690),
+        color=(185, 186, 188),
+        button=(443, 647, 465, 670),
     ),
 )
-ORDER_DESCENDING = ButtonWrapper(
-    name='ORDER_DESCENDING',
+MAIN_STAT_SELECT = ButtonWrapper(
+    name='MAIN_STAT_SELECT',
     share=Button(
-        file='./assets/share/item/relics/ORDER_DESCENDING.png',
-        area=(720, 649, 738, 667),
-        search=(700, 629, 758, 687),
-        color=(179, 180, 180),
-        button=(720, 649, 738, 667),
+        file='./assets/share/item/relics/MAIN_STAT_SELECT.png',
+        area=(832, 394, 872, 434),
+        search=(812, 374, 892, 454),
+        color=(236, 186, 105),
+        button=(832, 394, 872, 434),
     ),
 )
 SALVAGE = ButtonWrapper(
@@ -61,5 +105,45 @@ SALVAGE = ButtonWrapper(
         search=(936, 642, 1213, 675),
         color=(122, 115, 98),
         button=(1038, 646, 1062, 670),
+    ),
+)
+SALVAGE_ORDER_ASCENDING = ButtonWrapper(
+    name='SALVAGE_ORDER_ASCENDING',
+    share=Button(
+        file='./assets/share/item/relics/SALVAGE_ORDER_ASCENDING.png',
+        area=(720, 649, 737, 667),
+        search=(700, 629, 757, 687),
+        color=(176, 176, 178),
+        button=(720, 649, 737, 667),
+    ),
+)
+SALVAGE_ORDER_DESCENDING = ButtonWrapper(
+    name='SALVAGE_ORDER_DESCENDING',
+    share=Button(
+        file='./assets/share/item/relics/SALVAGE_ORDER_DESCENDING.png',
+        area=(720, 649, 738, 667),
+        search=(700, 629, 758, 687),
+        color=(179, 180, 180),
+        button=(720, 649, 738, 667),
+    ),
+)
+SET_SELECT = ButtonWrapper(
+    name='SET_SELECT',
+    share=Button(
+        file='./assets/share/item/relics/SET_SELECT.png',
+        area=(832, 146, 872, 186),
+        search=(812, 126, 892, 206),
+        color=(236, 186, 105),
+        button=(832, 146, 872, 186),
+    ),
+)
+SUB_STAT_SELECT = ButtonWrapper(
+    name='SUB_STAT_SELECT',
+    share=Button(
+        file='./assets/share/item/relics/SUB_STAT_SELECT.png',
+        area=(832, 580, 872, 620),
+        search=(812, 560, 892, 640),
+        color=(231, 183, 106),
+        button=(832, 580, 872, 620),
     ),
 )
--- tasks/item/relics.py
+++ tasks/item/relics.py
@@ -8,7 +8,10 @@ from tasks.item.ui import ItemUI
 
 class RelicsUI(ItemUI):
     def _is_in_salvage(self) -> bool:
-        return self.appear(ORDER_ASCENDING) or self.appear(ORDER_DESCENDING)
+        if self._is_in_relic_filter:
+            return False
+        else:
+            return self.appear(SALVAGE_ORDER_ASCENDING) or self.appear(SALVAGE_ORDER_DESCENDING)
 
     def salvage_exit(self, skip_first_screenshot=True):
         """
@@ -37,6 +40,8 @@ class RelicsUI(ItemUI):
 
     def salvage_relic(self, skip_first_screenshot=True) -> bool:
         """
+        Note:
+            As daily quest no longer demands salvaging relics, RelicsUI.salvage_relic is deprecated.
         Args:
             skip_first_screenshot:
 
@@ -80,9 +85,9 @@ class RelicsUI(ItemUI):
             if self.image_color_count(FIRST_RELIC_SELECTED, color=(245, 245, 245), threshold=221, count=300):
                 logger.info('First relic selected')
                 break
-            if self.appear_then_click(ORDER_DESCENDING, interval=2):
+            if self.appear_then_click(SALVAGE_ORDER_DESCENDING, interval=2):
                 continue
-            if interval.reached() and self.appear(ORDER_ASCENDING) \
+            if interval.reached() and self.appear(SALVAGE_ORDER_ASCENDING) \
                     and self.image_color_count(FIRST_RELIC, (233, 192, 108)):
                 self.device.click(FIRST_RELIC)
                 interval.reset()
@@ -105,3 +110,199 @@ class RelicsUI(ItemUI):
 
         self.salvage_exit()
         return True
+    
+    def _is_in_relic_filter(self) -> bool:
+        return self.appear(SET_SELECT) and self.appear(MAIN_STAT_SELECT)
+    
+    def item_goto_salvage(self, skip_first_screenshot=True) -> bool:
+        """item/relic -> salvage
+
+        Args:
+            skip_first_screenshot (bool, optional): Defaults to True.
+
+        Returns:
+            bool: True means reached salvage
+
+        Pages:
+            in: item/relic
+            out: salvage
+        """
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if self._is_in_salvage():
+                logger.info('_is_in_salvage')
+                return True
+            if self.appear_then_click(GOTO_SALVAGE, interval=2):
+                continue
+    
+    def salvage_goto_filter(self, skip_first_screenshot=True) -> bool:
+        """salvage -> salvage relic filter
+
+        Args:
+            skip_first_screenshot (bool, optional): Defaults to True.
+
+        Returns:
+            bool: True means reached salvage filter
+
+        Pages:
+            in: salvage
+            out: salvage filter
+        """
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if self._is_in_relic_filter():
+                logger.info('_is_in_relic_filter')
+                return True
+            if self.appear_then_click(GOTO_SALVAGE_FILTER, interval=2):
+                continue
+            
+    def relic_goto_enhance_filter(self, skip_first_screenshot=True) -> bool:
+        """item/relic -> relic enhance filter
+
+        Args:
+            skip_first_screenshot (bool, optional): Defaults to True.
+
+        Returns:
+            bool: True means reached enhance filter
+
+        Pages:
+            in: item/relic
+            out: enhance filter
+        """
+        # Enhance filter button is in a different location compared to salvage filter.
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if self._is_in_relic_filter():
+                logger.info('_is_in_relic_filter')
+                return True
+            if self.appear_then_click(GOTO_ENHANCE_FILTER, interval=2):
+                continue
+    
+    def relic_filter_confirm(self, skip_first_screenshot=True) -> bool:
+        """Click confirm button of the relic filter.
+
+        Args:
+            skip_first_screenshot (bool, optional): Defaults to True.
+
+        Returns:
+            bool: True means confirm button clicked.
+
+        Pages:
+            in: (enhance or salvage) relic filter
+            out: (enhance or salvage) relic selection
+        """
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if self._is_in_relic_filter():
+                logger.info('_is_in_relic_filter')
+            else:
+                logger.info('Not in relic filter panel, retrying...')
+                continue
+            if self.appear_then_click(FILTER_CONFIRM, interval=2):
+                return True
+    
+    def relic_filter_reset(self, skip_first_screenshot=True) -> bool:
+        """Click reset button of the relic filter.
+
+        Args:
+            skip_first_screenshot (bool, optional): Defaults to True.
+
+        Returns:
+            bool: True means reset button clicked.
+
+        Pages:
+            in: (enhance or salvage) relic filter
+            out: (enhance or salvage) relic filter
+        """
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if self._is_in_relic_filter():
+                logger.info('_is_in_relic_filter')
+            else:
+                logger.info('Not in relic filter panel, retrying...')
+                continue
+            if self.appear_then_click(FILTER_RESET, interval=2):
+                return True
+    
+    def salvage_selected(self, skip_first_screenshot=True) -> bool:
+        """Click salvage button.
+
+        Args:
+            skip_first_screenshot (bool, optional): Defaults to True.
+
+        Returns:
+            bool: True means salvage button clicked.
+
+        Pages:
+            in: relic salvage
+            out: relic salvage
+        """
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if self.reward_appear():
+                logger.info("Relic salvaged")
+                return True
+            if self.appear_then_click(SALVAGE, interval=2):
+                continue
+            if self.handle_popup_confirm():
+                continue
+
+    def salvage(self, skip_first_screenshot=True) -> bool:
+        """Salvages useless relics.
+
+        Args:
+            skip_first_screenshot (bool, optional): Defaults to True.
+
+        Returns:
+            bool: If success
+            
+        Pages:
+            in: Any
+            out: page_item
+        """
+        logger.hr('Salvage Relic', level=2)
+        self.item_goto(KEYWORDS_ITEM_TAB.Relics, wait_until_stable=False)
+        
+        self.item_goto_salvage(skip_first_screenshot)
+        
+        self.salvage_goto_filter()
+        
+        self.relic_filter_reset()
+        
+        #TODO: Set the filter
+        
+        self.relic_filter_confirm()
+        
+        self.salvage_selected()
+
+        self.salvage_exit()
+
+
+if __name__ == '__main__':
+    ui=RelicsUI('src',task='Alas')
+    ui.image_file = r"Screenshot_2024.05.24_21.36.38.511.png"
+    print(ui.appear(SUB_STAT_SELECT))

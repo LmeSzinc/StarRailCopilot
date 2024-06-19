508beb0482fc91c0c8a724bc6e62dcc4abb732a7
Zero <zeroeeau@gmail.com>
2024-06-19 20:48:53 +0800

    Fix: config redirection

--- module/config/config_updater.py
+++ module/config/config_updater.py
@@ -687,6 +687,7 @@ class ConfigUpdater:
         ('Dungeon.DungeonDaily.CalyxGolden', 'Dungeon.DungeonDaily.CalyxGolden', convert_20_dungeon),
         ('Dungeon.DungeonDaily.CalyxCrimson', 'Dungeon.DungeonDaily.CalyxCrimson', convert_20_dungeon),
         ('Dungeon.Planner.Item_Moon_Madness_Fang', 'Dungeon.Planner.Item_Moon_Rage_Fang'),
+        ('DailyQuest.AchievableQuest.Complete_Simulated_Universe_1_times', 'DailyQuest.AchievableQuest.Complete_Divergent_Universe_or_Simulated_Universe_1_times'),
         ('Rogue.RogueWorld.SimulatedUniverseElite', 'Rogue.RogueWorld.SimulatedUniverseFarm', convert_rogue_farm),
     ]
 

7adad31b33d813f25c8517ee84a2841a861cfcaa
Zebartin <16185081+Zebartin@users.noreply.github.com>
2024-06-23 17:10:07 +0800

    Upd: Add map plane RadiantFeldspar (#541)
    Co-authored-by: LmeSzinc <lmeszincsales@gmail.com>

--- dev_tools/keywords/map_plane.py
+++ dev_tools/keywords/map_plane.py
@@ -49,6 +49,13 @@ class GenerateMapPlane(GenerateKeyword):
                 world_id=-2,
                 plane_id=index + 1,
             )
+        domains = ['晖长石号', '开拓之尾号', '塔塔洛夫号', '飞翔时针号']
+        for index, domain in enumerate(domains):
+            yield dict(
+                text_id=to_id(domain),
+                world_id=3,
+                plane_id=index + 1,
+            )
 
         keywords = sorted(self.iter_planes(), key=lambda x: (x['world_id'], x['sort_id']))
         for keyword in keywords:

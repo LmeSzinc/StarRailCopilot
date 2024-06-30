5188b5423bd52e2eb5dea109aa073139262f282e
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-06-30 22:37:01 +0800

    Add: Switch among relic pages

---
+++ tasks/relics/assets/assets_relics_ui.py
@@ -0,0 +1,106 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+ENHANCE_CHECK = ButtonWrapper(
+    name='ENHANCE_CHECK',
+    cn=Button(
+        file='./assets/cn/relics/ui/ENHANCE_CHECK.png',
+        area=(1134, 645, 1185, 670),
+        search=(1114, 625, 1205, 690),
+        color=(179, 180, 180),
+        button=(1134, 645, 1185, 670),
+    ),
+    en=Button(
+        file='./assets/en/relics/ui/ENHANCE_CHECK.png',
+        area=(1115, 646, 1204, 671),
+        search=(1095, 626, 1224, 691),
+        color=(183, 184, 184),
+        button=(1115, 646, 1204, 671),
+    ),
+)
+ENHANCE_FILTER = ButtonWrapper(
+    name='ENHANCE_FILTER',
+    share=Button(
+        file='./assets/share/relics/ui/ENHANCE_FILTER.png',
+        area=(159, 650, 185, 676),
+        search=(139, 630, 205, 696),
+        color=(193, 195, 196),
+        button=(159, 650, 185, 676),
+    ),
+)
+ENHANCE_GOTO_SALVAGE = ButtonWrapper(
+    name='ENHANCE_GOTO_SALVAGE',
+    share=Button(
+        file='./assets/share/relics/ui/ENHANCE_GOTO_SALVAGE.png',
+        area=(685, 653, 705, 673),
+        search=(665, 633, 725, 693),
+        color=(140, 142, 143),
+        button=(685, 653, 705, 673),
+    ),
+)
+FILTER_CONFIRM = ButtonWrapper(
+    name='FILTER_CONFIRM',
+    cn=Button(
+        file='./assets/cn/relics/ui/FILTER_CONFIRM.png',
+        area=(1103, 651, 1151, 677),
+        search=(1083, 631, 1171, 697),
+        color=(180, 180, 180),
+        button=(1103, 651, 1151, 677),
+    ),
+    en=Button(
+        file='./assets/en/relics/ui/FILTER_CONFIRM.png',
+        area=(1081, 652, 1172, 677),
+        search=(1061, 632, 1192, 697),
+        color=(189, 189, 189),
+        button=(1081, 652, 1172, 677),
+    ),
+)
+FILTER_RESET = ButtonWrapper(
+    name='FILTER_RESET',
+    cn=Button(
+        file='./assets/cn/relics/ui/FILTER_RESET.png',
+        area=(887, 652, 936, 675),
+        search=(867, 632, 956, 695),
+        color=(160, 160, 160),
+        button=(887, 652, 936, 675),
+    ),
+    en=Button(
+        file='./assets/en/relics/ui/FILTER_RESET.png',
+        area=(879, 652, 946, 677),
+        search=(859, 632, 966, 697),
+        color=(185, 185, 185),
+        button=(879, 652, 946, 677),
+    ),
+)
+SALVAGE_CHECK_OFF = ButtonWrapper(
+    name='SALVAGE_CHECK_OFF',
+    share=Button(
+        file='./assets/share/relics/ui/SALVAGE_CHECK_OFF.png',
+        area=(1067, 646, 1091, 670),
+        search=(1002, 641, 1143, 675),
+        color=(54, 55, 48),
+        button=(1067, 646, 1091, 670),
+    ),
+)
+SALVAGE_CHECK_ON = ButtonWrapper(
+    name='SALVAGE_CHECK_ON',
+    share=Button(
+        file='./assets/share/relics/ui/SALVAGE_CHECK_ON.png',
+        area=(1067, 646, 1091, 670),
+        search=(1002, 641, 1143, 675),
+        color=(124, 116, 99),
+        button=(1067, 646, 1091, 670),
+    ),
+)
+SALVAGE_FILTER = ButtonWrapper(
+    name='SALVAGE_FILTER',
+    share=Button(
+        file='./assets/share/relics/ui/SALVAGE_FILTER.png',
+        area=(441, 645, 467, 671),
+        search=(421, 625, 487, 691),
+        color=(193, 194, 195),
+        button=(441, 645, 467, 671),
+    ),
+)

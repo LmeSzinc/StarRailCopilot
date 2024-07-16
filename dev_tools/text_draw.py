e14fd9ad4a9a29d015a48c62e291b97e661e6446
antecanis8 <42382878+antecanis8@users.noreply.github.com>
2024-07-16 18:40:02 +0800

    fix: Reuse get_bbox() and Change to single channel image

--- dev_tools/text_draw.py
+++ dev_tools/text_draw.py
@@ -5,27 +5,7 @@ import os
 from PIL import ImageFont, ImageDraw, Image
 from module.config.utils import read_file
 from module.logger import logger
-
-
-def crop_to_text_edge(original_image):
-    gray = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-
-    _, binary = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
-
-    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    x_min, y_min = float('inf'), float('inf')
-    x_max, y_max = float('-inf'), float('-inf')
-
-    for contour in contours:
-        x, y, w, h = cv2.boundingRect(contour)
-        x_min = min(x_min, x)
-        y_min = min(y_min, y)
-        x_max = max(x_max, x + w)
-        y_max = max(y_max, y + h)
-
-    cropped_image = original_image[y_min:y_max, x_min:x_max]
-    return cropped_image
+from module.base.utils import get_bbox_reversed,crop
 
 
 def split_string_at_last_space(input_string):
@@ -38,35 +18,31 @@ def split_string_at_last_space(input_string):
         part1 = input_string[:last_space_index]
         part2 = input_string[last_space_index + 1:]
 
-        return part1, part2
+        return part1
     else:
-        return input_string, ""
+        return input_string
 
 
 def text_image_generator(chars, names, font, output_path='./'):
     for char, name in zip(chars, names):
         image_path = os.path.join(output_path, name + '.png')
-        if (os.path.isfile(image_path)):
-            logger.info(f'{name} already exists, skipping')
-            continue
-        else:
-            part = ['', '']
-            part = split_string_at_last_space(char)
 
-            image = np.ones((50, 370, 3), dtype=np.uint8) * 255
+        part = split_string_at_last_space(char)
 
-            image_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        image = np.ones((50, 370), dtype=np.uint8) * 255
 
-            draw = ImageDraw.Draw(image_pil)
+        image_pil = Image.fromarray(image)
 
-            draw.text((2, 0.5), part[0], font=font, fill=(0, 0, 0))
-            draw.text((2, 25), part[1], font=font, fill=(0, 0, 0))
+        draw = ImageDraw.Draw(image_pil)
+        draw.text((2, 0.5), part, font=font, fill=0)
 
-            image = cv2.cvtColor(np.array(image_pil), cv2.COLOR_RGB2BGR)
+        image=np.array(image_pil)
+        crop_image = crop(image,get_bbox_reversed(image,70))
 
-            crop_image = crop_to_text_edge(image)
-            cv2.imencode('.png', crop_image)[1].tofile(image_path)
-            logger.info(f'{name} generated')
+        image_pil=Image.fromarray(crop_image)
+        image_pil.save(image_path)
+
+        logger.info(f'{name} generated')
 
 
 def relics_name_image_generator():

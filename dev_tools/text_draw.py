cdda84c983cece219c4ace462d0c8e79ad2c80e5
antecanis8 <42382878+antecanis8@users.noreply.github.com>
2024-07-18 19:14:33 +0800

    fix: Encapsulate as a class

--- dev_tools/text_draw.py
+++ dev_tools/text_draw.py
@@ -8,66 +8,52 @@ from module.logger import logger
 from module.base.utils import get_bbox_reversed,crop
 
 
-def split_string_at_last_space(input_string):
-    if all(ord(char) < 128 for char in input_string) and len(input_string) > 35:
-        last_space_index = input_string.rfind(' ')
+class TextImageGenerator:
+    def __init__(self, font_path="./dev_tools/zh-cn.ttf", font_size=20):
+        self.font_en = ImageFont.truetype(font_path, font_size)
+        self.font_cn = ImageFont.truetype(font_path, font_size)
 
-        if last_space_index == -1:
-            return input_string, ""
+    @staticmethod
+    def split_string_at_last_space(input_string):
+        if all(ord(char) < 128 for char in input_string) and len(input_string) > 35:
+            last_space_index = input_string.rfind(' ')
+            if last_space_index == -1:
+                return input_string, ""
+            part1 = input_string[:last_space_index]
+            part2 = input_string[last_space_index + 1:]
+            return part1
+        else:
+            return input_string
 
-        part1 = input_string[:last_space_index]
-        part2 = input_string[last_space_index + 1:]
+    def text_image_generator(self, chars, names, font, output_path='./'):
+        for char, name in zip(chars, names):
+            image_path = os.path.join(output_path, name + '.png')
+            part = self.split_string_at_last_space(char)
+            image = np.ones((50, 370), dtype=np.uint8) * 255
+            image_pil = Image.fromarray(image)
+            draw = ImageDraw.Draw(image_pil)
+            draw.text((2, 0.5), part, font=font, fill=0)
+            image = np.array(image_pil)
+            crop_image = crop(image, get_bbox_reversed(image, 70))
+            image_pil = Image.fromarray(crop_image)
+            image_pil.save(image_path)
+            logger.info(f'{name} generated')
 
-        return part1
-    else:
-        return input_string
-
-
-def text_image_generator(chars, names, font, output_path='./'):
-    for char, name in zip(chars, names):
-        image_path = os.path.join(output_path, name + '.png')
-
-        part = split_string_at_last_space(char)
-
-        image = np.ones((50, 370), dtype=np.uint8) * 255
-
-        image_pil = Image.fromarray(image)
-
-        draw = ImageDraw.Draw(image_pil)
-        draw.text((2, 0.5), part, font=font, fill=0)
-
-        image=np.array(image_pil)
-        crop_image = crop(image,get_bbox_reversed(image,70))
-
-        image_pil=Image.fromarray(crop_image)
-        image_pil.save(image_path)
-
-        logger.info(f'{name} generated')
-
-
-def relics_name_image_generator():
-    font_en = ImageFont.truetype("./dev_tools/zh-cn.ttf", 20)
-    font_cn = ImageFont.truetype("./dev_tools/zh-cn.ttf", 20)
-    output_path_cn = './assets/cn/relics/name/'
-    output_path_en = './assets/en/relics/name/'
-
-    with open('./tasks/relics/keywords/relics.py', 'r', encoding='utf-8') as file:
-        content = file.read()
-
-    cn_pattern = re.compile(r"cn='([^']*)'")
-    en_pattern = re.compile(r"en='([^']*)'")
-    name_pattern = re.compile(r"name='([^']*)'")
-
-    cn_values = cn_pattern.findall(content)
-    en_values = en_pattern.findall(content)
-    name_values = name_pattern.findall(content)
-
-    text_image_generator(cn_values, name_values, font=font_cn, output_path=output_path_cn)
-    text_image_generator(en_values, name_values, font=font_en, output_path=output_path_en)
-
+    def relics_name_image_generator(self, content_file='./tasks/relics/keywords/relics.py', output_path_cn='./assets/cn/relics/name/', output_path_en='./assets/en/relics/name/'):
+        with open(content_file, 'r', encoding='utf-8') as file:
+            content = file.read()
+        cn_pattern = re.compile(r"cn='([^']*)'")
+        en_pattern = re.compile(r"en='([^']*)'")
+        name_pattern = re.compile(r"name='([^']*)'")
+        cn_values = cn_pattern.findall(content)
+        en_values = en_pattern.findall(content)
+        name_values = name_pattern.findall(content)
+        self.text_image_generator(cn_values, name_values, font=self.font_cn, output_path=output_path_cn)
+        self.text_image_generator(en_values, name_values, font=self.font_en, output_path=output_path_en)
 
 if __name__ == '__main__':
-    relics_name_image_generator()
+    generator = TextImageGenerator()
+    generator.relics_name_image_generator()
 
 
 

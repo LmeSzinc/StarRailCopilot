431bd01f5236f8368d0ef5f81bfac92609148d78
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-07-23 23:59:08 +0800

    Fix: Homecoming account has a total of 42 doubled calyx

--- tasks/dungeon/event.py
+++ tasks/dungeon/event.py
@@ -86,7 +86,8 @@ class DungeonEvent(UI):
         # 3 is double relic
         # 12 is double calyx
         # 6 is double calyx on beginner account
-        if total not in [3, 6, 12]:
+        # 42 is double calyx on homecoming account
+        if total not in [3, 6, 12, 42]:
             logger.warning(f'Invalid double event remain')
             remain = 0
         return remain
@@ -123,7 +124,7 @@ class DungeonEvent(UI):
             if not ocr.is_format_matched(row.ocr_text):
                 continue
             remain, _, total = ocr.format_result(row.ocr_text)
-            if total in [3, 6, 12]:
+            if total in [3, 6, 12, 42]:
                 logger.attr('Double event remain at combat', remain)
                 return remain
         logger.warning('Double event appears but failed to get remain')

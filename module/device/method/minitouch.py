a548cee2b484d222d0df18dae882e7ad567fe862
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-10-10 21:42:12 +0800

    Fix: [ALAS] Missing wait after down event

--- module/device/method/minitouch.py
+++ module/device/method/minitouch.py
@@ -604,7 +604,7 @@ class Minitouch(Connection):
         points = insert_swipe(p0=p1, p3=p2)
         builder = self.minitouch_builder
 
-        builder.down(*points[0]).commit()
+        builder.down(*points[0]).commit().wait(10)
         builder.send()
 
         for point in points[1:]:
@@ -621,7 +621,7 @@ class Minitouch(Connection):
         points = insert_swipe(p0=p1, p3=p2, speed=20)
         builder = self.minitouch_builder
 
-        builder.down(*points[0]).commit()
+        builder.down(*points[0]).commit().wait(10)
         builder.send()
 
         for point in points[1:]:

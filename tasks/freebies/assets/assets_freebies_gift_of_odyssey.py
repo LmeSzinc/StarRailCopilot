6aed2331ee2f41679cd491f57c5f63e2126c1076
Hengyu <h3ngyu@gmail.com>
2024-12-05 16:36:38 +0800

    Feat: add Gift of Odyssey

---
+++ tasks/freebies/assets/assets_freebies_gift_of_odyssey.py
@@ -0,0 +1,35 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+EVENT_SELECTED = ButtonWrapper(
+    name='EVENT_SELECTED',
+    share=Button(
+        file='./assets/share/freebies/gift_of_odyssey/EVENT_SELECTED.png',
+        area=(1080, 376, 1144, 440),
+        search=(1060, 356, 1164, 460),
+        color=(206, 177, 134),
+        button=(1080, 376, 1144, 440),
+    ),
+)
+GET_REWARD_BUTTON = ButtonWrapper(
+    name='GET_REWARD_BUTTON',
+    share=Button(
+        file='./assets/share/freebies/gift_of_odyssey/GET_REWARD_BUTTON.png',
+        area=(647, 397, 665, 415),
+        search=(626, 243, 1212, 631),
+        color=(121, 120, 105),
+        button=(647, 397, 665, 415),
+    ),
+)
+OCR_EVENT = ButtonWrapper(
+    name='OCR_EVENT',
+    share=Button(
+        file='./assets/share/freebies/gift_of_odyssey/OCR_EVENT.png',
+        area=(34, 73, 222, 680),
+        search=(14, 53, 242, 700),
+        color=(97, 96, 112),
+        button=(34, 73, 222, 680),
+    ),
+)
---
+++ tasks/freebies/keywords/__init__.py
@@ -0,0 +1,2 @@
+import tasks.freebies.keywords.gift_of_odyssey as KEYWORDS_FREEBIES_GIFT_OF_ODYSSEY
+from tasks.freebies.keywords.classes import GiftOfOdysseyEvent

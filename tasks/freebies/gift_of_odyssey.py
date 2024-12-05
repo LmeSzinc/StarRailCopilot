0cef368bbbaf070935662298d8c2e8c268d1d7a7
Hengyu <h3ngyu@gmail.com>
2024-12-05 16:36:38 +0800

    Feat: add Gift of Odyssey

---
+++ tasks/freebies/gift_of_odyssey.py
@@ -0,0 +1,62 @@
+from module.base.timer import Timer
+from module.logger import logger
+from module.ocr.ocr import Ocr
+from tasks.base.page import page_event
+from tasks.base.ui import UI
+from tasks.freebies.assets.assets_freebies_gift_of_odyssey import OCR_EVENT, EVENT_SELECTED, GET_REWARD_BUTTON
+from tasks.freebies.keywords import GiftOfOdysseyEvent
+
+
+class GiftofOdyssey(UI):
+
+    def run(self):
+        logger.hr("Gift of Odyssey", level=1)
+        self.ui_ensure(page_event)
+        if self._select_event():
+            logger.info("Event selected")
+            self._get_reward()
+
+    def _select_event(self):
+        logger.info("Selecting Gift of Odyssey event")
+        ocr = Ocr(OCR_EVENT)
+        # When Gift of Odyssey event is the first event, the ocr results will be effected due to the animation of selected event
+        timer = Timer(0.5, count=1).start()
+        while 1:
+            self.device.screenshot()
+            results = ocr.matched_ocr(self.device.image, GiftOfOdysseyEvent)
+            if len(results) == 0 and timer.reached():
+                logger.info("Event not found")
+                return False
+            if len(results) == 1:
+                break
+
+        skip_first_screenshot = True
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if self.appear(EVENT_SELECTED):
+                break
+            if self.device.click(results[0]):
+                continue
+
+        return True
+
+    def _get_reward(self):
+        logger.info("Getting reward")
+        skip_first_screenshot = True
+        interval = Timer(2)
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if not self.handle_reward() and not self.appear(GET_REWARD_BUTTON):
+                logger.info("No more reward to get")
+                break
+            if interval.reached():
+                if self.appear_then_click(GET_REWARD_BUTTON):
+                    interval.reset()

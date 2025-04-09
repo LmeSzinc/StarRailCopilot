a3281fa6025f402d5295cd676e789e3db67acf52
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-04-09 19:38:34 +0800

    Fix: Add missing assets py

---
+++ tasks/combat/assets/assets_combat_relics.py
@@ -0,0 +1,15 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+COMBAT_RELIC_ENTER = ButtonWrapper(
+    name='COMBAT_RELIC_ENTER',
+    share=Button(
+        file='./assets/share/combat/relics/COMBAT_RELIC_ENTER.png',
+        area=(830, 654, 855, 671),
+        search=(810, 634, 875, 691),
+        color=(109, 107, 108),
+        button=(830, 654, 855, 671),
+    ),
+)

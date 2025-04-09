ee7ff46adf6e371cbdcf75c3c235858a80441634
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-04-09 19:10:09 +0800

    Upd: Set combat wave and get wave cost (#794)

--- tasks/combat/assets/assets_combat_prepare.py
+++ tasks/combat/assets/assets_combat_prepare.py
@@ -34,49 +34,59 @@ OCR_WAVE_COST = ButtonWrapper(
     name='OCR_WAVE_COST',
     share=Button(
         file='./assets/share/combat/prepare/OCR_WAVE_COST.png',
-        area=(845, 649, 891, 667),
-        search=(825, 629, 911, 687),
-        color=(50, 53, 57),
-        button=(845, 649, 891, 667),
+        area=(871, 649, 903, 667),
+        search=(851, 629, 923, 687),
+        color=(194, 192, 194),
+        button=(871, 649, 903, 667),
     ),
 )
 OCR_WAVE_COUNT = ButtonWrapper(
     name='OCR_WAVE_COUNT',
     share=Button(
         file='./assets/share/combat/prepare/OCR_WAVE_COUNT.png',
-        area=(911, 555, 1151, 581),
-        search=(891, 535, 1171, 601),
+        area=(911, 533, 1151, 559),
+        search=(891, 513, 1171, 579),
         color=(32, 34, 37),
-        button=(911, 555, 1151, 581),
+        button=(911, 533, 1151, 559),
+    ),
+)
+WAVE_COST_CHECK = ButtonWrapper(
+    name='WAVE_COST_CHECK',
+    share=Button(
+        file='./assets/share/combat/prepare/WAVE_COST_CHECK.png',
+        area=(830, 645, 856, 671),
+        search=(815, 634, 963, 682),
+        color=(194, 185, 219),
+        button=(830, 645, 856, 671),
     ),
 )
 WAVE_MINUS = ButtonWrapper(
     name='WAVE_MINUS',
     share=Button(
         file='./assets/share/combat/prepare/WAVE_MINUS.png',
-        area=(825, 577, 858, 599),
-        search=(805, 557, 878, 619),
+        area=(825, 555, 858, 577),
+        search=(805, 535, 878, 597),
         color=(239, 239, 239),
-        button=(825, 577, 858, 599),
+        button=(825, 555, 858, 577),
     ),
 )
 WAVE_PLUS = ButtonWrapper(
     name='WAVE_PLUS',
     share=Button(
         file='./assets/share/combat/prepare/WAVE_PLUS.png',
-        area=(1204, 578, 1236, 598),
-        search=(1184, 558, 1256, 618),
+        area=(1204, 556, 1236, 576),
+        search=(1184, 536, 1256, 596),
         color=(229, 229, 229),
-        button=(1204, 578, 1236, 598),
+        button=(1204, 556, 1236, 576),
     ),
 )
 WAVE_SLIDER = ButtonWrapper(
     name='WAVE_SLIDER',
     share=Button(
         file='./assets/share/combat/prepare/WAVE_SLIDER.png',
-        area=(911, 585, 1150, 591),
-        search=(891, 565, 1170, 611),
+        area=(911, 563, 1150, 569),
+        search=(891, 543, 1170, 589),
         color=(167, 118, 68),
-        button=(911, 585, 1150, 591),
+        button=(911, 563, 1150, 569),
     ),
 )

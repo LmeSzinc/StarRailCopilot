31187c7bf70dac0d3d93d5d996b7d6f656ad31c7
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-04-23 03:21:26 +0800

    Fix: Ornament route should inherit from base route, not the rogue one

--- route/ornament/Combat/Amphoreus_BloodbathedBattlefrontCastrumKremnos_F1OE.py
+++ route/ornament/Combat/Amphoreus_BloodbathedBattlefrontCastrumKremnos_F1OE.py
@@ -1,6 +1,6 @@
 from tasks.map.control.waypoint import Waypoint
 from tasks.map.keywords.plane import Amphoreus_BloodbathedBattlefrontCastrumKremnos
-from tasks.rogue.route.base import RouteBase
+from tasks.ornament.route_base import RouteBase
 
 
 class Route(RouteBase):
@@ -11,10 +11,8 @@ class Route(RouteBase):
         | -------- | ------------------------- | --------- | -------- |
         | spawn    | Waypoint((155.5, 669.3)), | 4.2       | 4        |
         | enemy    | Waypoint((155.9, 627.4)), | 278.1     | 1        |
-        | exit_    | Waypoint((155.9, 627.4)), | 278.1     | 1        |
         """
         self.map_init(plane=Amphoreus_BloodbathedBattlefrontCastrumKremnos, floor="F1OE", position=(155.5, 669.3))
-        self.register_domain_exit(Waypoint((155.9, 627.4)), end_rotation=1)
         enemy = Waypoint((155.9, 627.4))
         # ===== End of generated waypoints =====
 
--- route/ornament/Combat/Amphoreus_StrifeRuinsCastrumKremnos_F1OE.py
+++ route/ornament/Combat/Amphoreus_StrifeRuinsCastrumKremnos_F1OE.py
@@ -1,6 +1,6 @@
 from tasks.map.control.waypoint import Waypoint
 from tasks.map.keywords.plane import Amphoreus_StrifeRuinsCastrumKremnos
-from tasks.rogue.route.base import RouteBase
+from tasks.ornament.route_base import RouteBase
 
 
 class Route(RouteBase):
@@ -11,10 +11,8 @@ class Route(RouteBase):
         | -------- | ------------------------- | --------- | -------- |
         | spawn    | Waypoint((373.2, 317.4)), | 4.2       | 1        |
         | enemy    | Waypoint((368.4, 281.3)), | 11.2      | 4        |
-        | exit_    | Waypoint((368.4, 281.3)), | 11.2      | 4        |
         """
         self.map_init(plane=Amphoreus_StrifeRuinsCastrumKremnos, floor="F1OE", position=(373.2, 317.4))
-        self.register_domain_exit(Waypoint((368.4, 281.3)), end_rotation=4)
         enemy = Waypoint((368.4, 281.3))
         # ===== End of generated waypoints =====
 

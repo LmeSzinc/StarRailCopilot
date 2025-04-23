68bb08fdf14d8d057236bb77a26bdaca0c20445a
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-04-23 09:09:38 +0800

    Fix: Add assets_ornament_special.py

---
+++ tasks/ornament/assets/assets_ornament_special.py
@@ -0,0 +1,15 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+Amphoreus_StrifeRuinsCastrumKremnos_F1OE_X373Y317 = ButtonWrapper(
+    name='Amphoreus_StrifeRuinsCastrumKremnos_F1OE_X373Y317',
+    share=Button(
+        file='./assets/share/ornament/special/Amphoreus_StrifeRuinsCastrumKremnos_F1OE_X373Y317.png',
+        area=(93, 125, 141, 151),
+        search=(73, 105, 161, 171),
+        color=(95, 116, 119),
+        button=(93, 125, 141, 151),
+    ),
+)

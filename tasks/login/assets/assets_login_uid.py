67ad51345bdfb57869c12184085fa909ebcb138f
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-06-18 18:20:23 +0800

    Dev: Add UIDHandler, not used yet

---
+++ tasks/login/assets/assets_login_uid.py
@@ -0,0 +1,15 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+OCR_UID = ButtonWrapper(
+    name='OCR_UID',
+    share=Button(
+        file='./assets/share/login/uid/OCR_UID.png',
+        area=(24, 690, 149, 706),
+        search=(4, 670, 169, 720),
+        color=(145, 145, 156),
+        button=(24, 690, 149, 706),
+    ),
+)

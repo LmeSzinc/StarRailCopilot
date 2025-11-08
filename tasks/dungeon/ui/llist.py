b8de590adc0162418f83b6e306cb932029e81a56
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-11-09 01:10:03 +0800

    Fix: Dungeon name Rusted_Crypt_of_the_Iron_Carcass

--- tasks/dungeon/ui/llist.py
+++ tasks/dungeon/ui/llist.py
@@ -69,6 +69,9 @@ class OcrDungeonName(Ocr):
             # 9支援仓段
             for word in 'Q9α':
                 result = result.removeprefix(word)
+            # 铁的锈家 -> 铁骸的锈冢
+            result = result.replace('锈家', '锈冢')
+            result = result.replace('铁的', '铁骸的')
         elif self.lang == 'en':
             # O Supply Zone
             result = re.sub(r'^[Oo0] S', 'S', result)

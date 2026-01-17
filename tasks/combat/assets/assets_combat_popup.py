bb435ce49ab51810acb352ebcee204d84b57d28d
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2026-01-17 22:26:37 +0800

    Fix: Handle buff popups in Echo_of_War_Rusted_Crypt_of_the_Iron_Carcass

---
+++ tasks/combat/assets/assets_combat_popup.py
@@ -0,0 +1,25 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+Rusted_Crypt_1 = ButtonWrapper(
+    name='Rusted_Crypt_1',
+    share=Button(
+        file='./assets/share/combat/popup/Rusted_Crypt_1.png',
+        area=(982, 100, 1016, 124),
+        search=(962, 80, 1036, 144),
+        color=(43, 74, 70),
+        button=(982, 100, 1016, 124),
+    ),
+)
+Rusted_Crypt_2 = ButtonWrapper(
+    name='Rusted_Crypt_2',
+    share=Button(
+        file='./assets/share/combat/popup/Rusted_Crypt_2.png',
+        area=(1092, 355, 1124, 379),
+        search=(1072, 335, 1144, 399),
+        color=(46, 79, 74),
+        button=(1092, 355, 1124, 379),
+    ),
+)
